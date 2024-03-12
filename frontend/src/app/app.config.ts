bf50140de211c09929266ec7eb47fc29545ce4a7
alySantolini <alice.santolini2014@gmail.com>
2024-03-12 17:43:38 +0100

    inserimento servizio login

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,8 +1,5 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { FormsModule } from '@angular/forms';
-
-
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -4,7 +4,6 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
