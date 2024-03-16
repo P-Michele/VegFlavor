4fe488568babddafcbcf34a055abd8f02edf239e
Vabele <valerio.belenchia@studenti.unicam.it>
2024-03-16 17:53:14 +0100

    fixed jwt extraction pattern

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -2,11 +2,11 @@ const jwt = require("jsonwebtoken");
 const jwtConfig = require("../configs/jwtConfig");
 
 const verifyToken = (req, res, next) => {
-  const token = req.header("Authorization");
-  if (!token) {
+  const authHeader = req.header("Authorization");
+  if (!authHeader) {
     return res.status(401).json({ message: 'Token missing' });
   }
-
+  const token = authHeader.split(' ')[1];
   jwt.verify(token, jwtConfig.secret, (error, decoded) => {
     if (error) {
       res.status(401).json({ message: error.message });
