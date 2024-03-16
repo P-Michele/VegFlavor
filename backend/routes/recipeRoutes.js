b1e9bfaeef9859744b28d1ca509b0517a8f80531
Vabele <valerio.belenchia@studenti.unicam.it>
2024-03-16 17:51:43 +0100

    added addRecipe - v1

--- backend/routes/recipeRoutes.js
+++ backend/routes/recipeRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const {getRecipes, getRecipe} = require("../controllers/recipeController");
+const {getRecipes, getRecipe, addRecipe} = require("../controllers/recipeController");
+const { verifyToken } = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 
 router.get("/", getRecipes);
 router.get("/:id", getRecipe);
+router.post("/", verifyToken, addRecipe);
 
 module.exports = router;
