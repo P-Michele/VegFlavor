d9a919a28bba67b58d39cfe8c27e817fc15d1445
Vabele <valerio.belenchia@studenti.unicam.it>
2024-03-23 21:06:18 +0100

    fixed fileDeletion

--- backend/services/fileDeletionService.js
+++ backend/services/fileDeletionService.js
@@ -2,17 +2,8 @@ const fs = require('fs').promises;
 const path = require('path');
 
 function deleteFile(filename) {
-    return new Promise((resolve, reject) => {
-        const filePath = path.join(__dirname, '..', 'uploads', filename);
-        
-        fs.unlink(filePath, (error) => {
-            if (error) {
-                reject(error);
-            } else {
-                resolve();
-            }
-        });
-    });
+    const filePath = path.join(__dirname, '..', 'uploads', filename);
+    return fs.unlink(filePath);
 }
 
 module.exports = { deleteFile };
